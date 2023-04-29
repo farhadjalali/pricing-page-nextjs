59ce367d3c21b272090e372abdb93f6a65756fc2
Farhad <farhad@info.nl>
2023-04-29 10:24:18 +0200

    feat: add fonts

---
+++ src/styles/globals.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Sofia Pro';
+    src: local('Sofia Pro'), url(./fonts/SourceSansPro-Regular.ttf) format('truetype');
+  }
+
+  @font-face {
+    font-family: 'Sofia Pro';
+    src: local('Sofia Pro'), url(./fonts/SourceSansPro-Bold.ttf) format('truetype');
+    font-weight: bold;
+  }
+
+  @font-face {
+    font-family: 'Sofia Pro';
+    src: local('Sofia Pro'), url(./fonts/SourceSansPro-Light.ttf) format('truetype');
+    font-weight: 100;
+  }
+
+  html, body {
+    height: 100%;
+    margin: 0;
+    color-scheme: "light";
+    font-family: 'Sofia Pro', sans-serif;
+  }
+`;
