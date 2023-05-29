57fff3548bc9f4cc5e79305efb6539b3871d6294
Farhad <farhad@info.nl>
2023-05-29 20:21:11 +0200

    chore: something

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import styled from "styled-components";
+import Image from "next/image";
 
 const Page = styled.section`
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-wrap: wrap;
   height: 100%;
   width: 100%;
   left: 0;
@@ -14,7 +16,15 @@ const Page = styled.section`
 const Home: React.FC = () => {
   return (
     <Page>
-      <a href="/pricing">Go to pricing page</a>
+      <p>
+        <Image src="/images/mina.jpeg" alt={""} width={32} height={32} />
+        <br />
+        You are currently visiting the Mina Office official website. We regret
+        to inform you that we are no longer in business!
+        <br />
+        If you need any further information, you can contact us via email
+        (we@minaoffice.com)
+      </p>
     </Page>
   );
 };
